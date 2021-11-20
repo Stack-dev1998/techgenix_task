d8348640f54c3071534bb396ff03cceef97d0d2a
Stack-dev1998 <64778276+Stack-dev1998@users.noreply.github.com>
2021-11-20 08:58:31 +0500

    Delete task directory

--- task/frontend/src/components/employee/allRequests.js
+++
@@ -1,9 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { NavLink, Outlet } from "react-router-dom";
-import { Nav } from "react-bootstrap";
-
-export default function AllRequests() {
-  return (
-    <div>All request</div>
-  );
-}
